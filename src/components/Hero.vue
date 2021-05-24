3fc27468104af48af65d8835894ad0a5f8a7eaee
Boye Adeotan <55053080+Rippyblogger@users.noreply.github.com>
2021-05-24 11:13:29 +0400

    hero commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,74 @@
+<template>
+	<div class="hero-div">
+		<div class="hero-text">
+			<h1 class="faded">NEW</h1>
+			<h1 class="faded-below">ARRIVALS</h1>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Hero",
+    data() {
+        return {
+            Active: true
+        }
+    },
+};
+</script>
+
+<style scoped>
+.hero-div {
+	background-image: url("../assets/Daco.png");
+	margin: 0;
+    padding: 0;
+	height: 36rem;
+	background-repeat: no-repeat;
+	background-size: 41rem auto;
+	background-position: 40%;
+}
+
+.hero-text{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 4rem;
+}
+
+.faded {
+	font-size: 12rem;
+    padding: 1rem;
+    margin: 0;
+    margin-right: 50%;
+    align-self: center;
+}
+
+.faded-below {
+	font-size: 10rem;
+    padding: 0.5rem;
+    margin: 0;
+    margin-left: 30%;
+    margin-top: 4rem;
+    align-self: center;
+}
+
+
+
+@media only screen and (max-width : 767px){
+    .faded{
+        margin: 0;
+        margin-top: -1rem;
+        font-size: 10rem;
+    }
+
+    .faded-below{
+        margin: 0;
+        margin-top: 7rem;
+        padding: 0;
+        font-size: 4.7rem;
+    }
+}
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,145 @@
+<template>
+	<div class="hello">
+		<div class="header">
+			<p>Irorun Shop</p>
+			<img
+        @click="navSwitch"
+				v-if="displayed"
+				class="burger"
+				src="../assets/icons8-menu-90.png"
+				alt=""
+			/>
+		</div>
+
+		<div class="navi">
+			<hr />
+			<ul class="flex-cont" v-if="navShow">
+				<li><a href="">HOME</a></li>
+				<li><a href="">ALL</a></li>
+				<li><a href="">WOMEN</a></li>
+				<li><a href="">MEN</a></li>
+				<li><a href="">CART</a></li>
+			</ul>
+			<hr />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Navbar",
+	props: {
+		msg: String,
+	},
+	data() {
+		return {
+			displayed: false,
+			Width: window.innerWidth,
+      navShow: true,
+		};
+	},
+	created() {
+        window.addEventListener('resize', this.handleResize);
+        this.handleResize();
+    },
+    destroyed() {
+        window.removeEventListener('resize', this.handleResize);
+    },
+    methods: {
+        handleResize() {
+            this.Width = window.innerWidth;
+            if(this.Width <= 767){
+              this.displayed = !this.displayed
+              this.navShow = !this.navShow
+            }else if(this.Width > 767){
+              this.displayed = false
+            }
+        },
+
+        navSwitch(){
+          this.navShow = !this.navShow
+        }
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.hello{
+  box-sizing: border-box;
+}
+
+.header {
+	font-weight: 300;
+	font-size: 2rem;
+	margin: 30px;
+	color: #d40406;
+}
+
+li {
+	margin: 0 10px;
+	list-style-type: none;
+	padding: 0 2rem;
+	border-left: 1px solid black;
+	border-right: 1px solid black;
+}
+
+li a {
+	text-decoration: none;
+	color: black;
+}
+
+li a:hover {
+	color: #d62828;
+}
+
+.flex-cont {
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	list-style-type: none;
+	padding: 0;
+	font-weight: 300;
+	font-size: 0.9rem;
+}
+
+hr {
+	color: black;
+	width: 80%;
+	display: block;
+}
+
+@media only screen and (max-width: 767px) {
+	.header {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+    align-items: center;
+	}
+
+	.flex-cont {
+		flex-direction: column;
+	}
+
+	.burger {
+    height: 2rem;
+	}
+
+  .burger:hover{
+    border: 1px solid #d62828;
+    padding: 2px;
+    border-radius: 5px;
+  }
+
+	hr {
+		display: none;
+	}
+
+	li {
+		border-left: none;
+		border-right: none;
+		padding: 0.5rem;
+	}
+}
+</style>
