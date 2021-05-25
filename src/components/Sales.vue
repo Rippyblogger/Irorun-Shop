47360b65c89fe5f41f1d0e9b1a4ac2e032f5d55d
Boye Adeotan <55053080+Rippyblogger@users.noreply.github.com>
2021-05-25 12:06:34 +0400

    sales section done

--- src/components/Sales.vue
+++ src/components/Sales.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="background">
         <div class="text">
-            <h1>NEW</h1>
-            <p>Men Sneakers</p>
-            <p>Collection</p>
+            <p class="bigger">50%</p>
+            <p class="smaller1">Storewide Sale</p>
+            <p class="smaller2">Summer</p>
+            <p class="smallest">ALL ACCESSORIES</p>
         </div>
     </div>
 </template>
@@ -35,6 +36,70 @@
     display: flex;
     justify-content: flex-start;
     flex-direction: column;
+    position: relative;
+}
+
+.bigger{
+    font-weight: 500;
+    font-size: 17rem;
+    opacity: 0.4;
+    color: grey;
+    left: 10%;
+    position: absolute;
+    top: -6.5rem;
+}
+
+.smaller1{
+    font-weight: 300;
+    font-size: 4rem;
+    position: absolute;
+    left: 11.5%;
+    top: 12rem;
+    color: #188B46;
+    /* word-spacing: 0.5rem; */
+}
+
+.smaller2{
+    font-weight: 300;
+    font-size: 4rem;
+    position: absolute;
+    left: 29rem;
+    top: 17rem;
+}
+
+.smallest{
+    font-weight: 100;
+    font-size: 1.5rem;
+    position: absolute;
+    left: 20rem;
+    top: 26rem;
+}
+
+@media only screen and (max-width: 767px){
+    .bigger{
+        left: -1rem;
+        top: 14rem;
+        font-size: 13rem;
+    }
+
+    .smaller1{
+        top: 28rem;
+        font-size: 2.5rem;
+        /* right: 7rem; */
+    }
+
+    .smaller2{
+        left: 6.5rem;
+        top: 29rem;
+        font-size: 4rem;
+    }
+
+    .smallest{
+    font-size: 1.5rem;
+    left: 0;
+    left: 5rem;
+    top: 40rem;
+}
 }
 
 </style>
